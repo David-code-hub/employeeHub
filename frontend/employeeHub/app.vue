621d5dd7f420ca1e39e7c412b8da01cff331f24e
David <david@rlabs.org>
2024-05-28 23:41:37 +0200

    add: nuxt app started and installed ui

---
+++ frontend/employeeHub/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <UAlert
+      icon="i-heroicons-command-line"
+      description="You can add components to your app using the cli."
+      title="Heads up!"
+    />
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
