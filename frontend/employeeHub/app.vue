515e9758028f300d367a24188defaa542f456f79
David <david@rlabs.org>
2024-07-11 20:56:58 +0200

    add: pages

--- frontend/employeeHub/app.vue
+++ frontend/employeeHub/app.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <UAlert
-      icon="i-heroicons-command-line"
-      description="You can add components to your app using the cli."
-      title="Heads up!"
-    />
-    <!-- <NuxtWelcome /> -->
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ frontend/employeeHub/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full 2xl:w-[1440px]">
+    <div class="grid grid-cols-3"></div>
+    <h1 class="text-xl text-black">Home page</h1>
+  </div>
+</template>
+<script setup lang="ts"></script>
